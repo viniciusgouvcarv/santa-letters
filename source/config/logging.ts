84922c1ee6d39422c8751238020658f1a4dc8de1
viniciusgouvcarv <viniciusgouvcarv@gmail.com>
2021-03-05 22:28:33 -0300

    Refactoring Logging file

--- source/config/logging.ts
+++ source/config/logging.ts
@@ -2,25 +2,25 @@ const getTimeStamp = (): string => {
     return new Date().toISOString();
 };
 
-const info = (namespace: string, message: string, object: any) => {
+const info = (namespace: string, message: string, object?: any) => {
     object
         ? console.log(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`, object)
         : console.log(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`);
 };
 
-const warn = (namespace: string, message: string, object: any) => {
+const warn = (namespace: string, message: string, object?: any) => {
     object
         ? console.warn(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`, object)
         : console.warn(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`);
 };
 
-const error = (namespace: string, message: string, object: any) => {
+const error = (namespace: string, message: string, object?: any) => {
     object
         ? console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`, object)
         : console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`);
 };
 
-const debug = (namespace: string, message: string, object: any) => {
+const debug = (namespace: string, message: string, object?: any) => {
     object
         ? console.debug(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`, object)
         : console.debug(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`);
